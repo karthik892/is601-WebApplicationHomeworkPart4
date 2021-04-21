47211aec02175ca121974ed348dcd4ccca5d3103
Karthik Sankaran <karthik892@users.noreply.github.com>
2021-04-21 15:14:43 -0400

    Added API code

--- app/app.py
+++ app/app.py
@@ -71,5 +71,61 @@ def form_delete_post(city_id):
     mysql.get_db().commit()
     return redirect("/", code=302)
 
+@app.route('/api/mlb', methods=['GET'])
+def api_mlb_list() -> str:
+    cursor = mysql.get_db().cursor()
+    cursor.execute('SELECT * FROM mlb_players')
+    result = cursor.fetchall()
+    json_result = json.dumps(result)
+    resp = Response(json_result, status=200, mimetype='application/json')
+    return resp
+
+@app.route('/api/mlb/<int:mlb_id>', methods=['GET'])
+def api_mlb_view(mlb_id) -> str:
+    cursor = mysql.get_db().cursor()
+    cursor.execute('SELECT * FROM mlb_players WHERE id=%s', mlb_id)
+    result = cursor.fetchall()
+    json_result = json.dumps(result);
+    resp = Response(json_result, status=200, mimetype='application/json')
+    return resp
+
+@app.route('/api/mlb/<int:mlb_id>', methods=['PUT'])
+def api_mlb_save(mlb_id) -> str:
+    cursor = mysql.get_db().cursor()
+    content = request.json
+    inputData = (content['fldName'], content['fldTeam'], content['fldPosition'],
+                 content['fldWeight'], content['fldHeight'],
+                 content['fldAge'], mlb_id)
+    sql_update_query = """UPDATE mlb_players t SET t.Name = %s, t.Team = %s, t.Position = %s, t.Weight_lbs = 
+    %s, t.Height_inches = %s, t.Age = %s WHERE t.id = %s """
+    cursor.execute(sql_update_query, inputData)
+    mysql.get_db().commit()
+    resp = Response(status=200, mimetype='application/json')
+    return resp
+
+@app.route('/api/mlb/new', methods=['POST'])
+def api_mlb_add() -> str:
+
+    content = request.json
+
+    cursor = mysql.get_db().cursor()
+    inputData = (content['fldName'], content['fldTeam'], content['fldPosition'],
+                 content['fldWeight'], content['fldHeight'],
+                 content['fldAge'])
+    sql_insert_query = """INSERT INTO mlb_players (`Name`, `Team`, `Position`, `Height_inches`, `Weight_lbs`, `Age`) VALUES (%s, %s,%s, %s,%s, %s) """
+    cursor.execute(sql_insert_query, inputData)
+    mysql.get_db().commit()
+    resp = Response(status=201, mimetype='application/json')
+    return resp
+
+@app.route('/api/mlb/<int:mlb_id>', methods=['DELETE'])
+def api_delete(mlb_id) -> str:
+    cursor = mysql.get_db().cursor()
+    sql_delete_query = """DELETE FROM mlb_players WHERE id = %s """
+    cursor.execute(sql_delete_query, mlb_id)
+    mysql.get_db().commit()
+    resp = Response(status=200, mimetype='application/json')
+    return resp
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
